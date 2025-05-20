2685a756962d74eaf7416456c2acd523c2944723
nhorto <155685655+nhorto@users.noreply.github.com>
2025-05-20 17:14:45 -0400

    Delete polyfills.js

--- polyfills.js
+++
@@ -1,7 +0,0 @@
-import 'react-native-polyfill-globals/auto';
-import 'event-target-polyfill';
-import 'react-native-url-polyfill/auto';
-
-// Optional: You can add specific polyfills if needed
-global.process = require('process');
-global.Buffer = require('buffer').Buffer;
